22d78e82e48367adc8c857983ed0d99fa94f5157
Eclipse <virgile.henry@0211@gmail.com>
2023-08-06 11:15:57 +0200

    slight perf increase with inline of generated enum funcs

--- src/ecs/component_iterator/query_state.rs
+++ src/ecs/component_iterator/query_state.rs
@@ -2,6 +2,7 @@
 macro_rules! query_state_funcs {
     ($name:ty, $first:path, $($elems:path),*) => {
         impl $name {
+            #[inline]
             pub fn next_state(self) -> Self {
                 match self {
                     $first => 
@@ -10,6 +11,7 @@ macro_rules! query_state_funcs {
                 }
             }
     
+            #[inline]
             pub fn reset_state() -> Self {
                 $first
             }
